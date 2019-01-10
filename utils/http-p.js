3245648266f61e92d1398c68151d0837cfea0266
123697@qq.com <123697@qq.com>
2019-01-10 20:24:18 +0800

    fix:remaining question.

--- utils/http-p.js
+++ utils/http-p.js
@@ -3,11 +3,11 @@ import {API} from './api.js'
 class HTTP {
   request({ url, data = {}, method = 'GET' }) {
     return new Promise((resolve, reject) => {
-      this._request(url, resolve, reject, data, method)
+      this.selfRequest(url, resolve, reject, data, method)
     })
   }
   //http 请求类, 当noRefech为true时，不做未授权重试机制
-  _request(url, resolve, reject, data = {}, method = 'GET') {
+  selfRequest(url, resolve, reject, data = {}, method = 'GET') {
     wx.request({
       url: API.baseUrl + url,
       data: data,
@@ -20,8 +20,7 @@ class HTTP {
         // 判断以2（2xx)开头的状态码为正确
         // 异常不要返回到回调中，就在request中处理，记录日志并showToast一个统一的错误即可
         var code = res.statusCode.toString();
-        var startChar = code.charAt(0);
-        if (startChar == '2') {
+        if (code.startsWith('2')) {
           resolve(res.data)
         } else {
           reject()
