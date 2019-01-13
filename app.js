9d8fc884e2b32b8539d4218e1592c46a86a82136
Spock <372842848@qq.com>
2019-01-13 16:02:40 +0800

    style:forget

--- app.js
+++ app.js
@@ -21,17 +21,15 @@ App({
   getLoginParamer(res) {
     http_p.request({
       url: 'users/login?code=' + res.code,
-      isLoading:false
+      isLoading: false
     }).then(res => {
-      console.log("发送resCode", res.data)
-      let user = res.data;
-
-      this.checkPowerStatus(user)
+      console.log("get login data:", res.data)
+      this.checkPowerStatus();
     })
   },
   // 判断是否授权
   // 获取用户信息并判断是否相同 并更新
-  checkPowerStatus(user) {
+  checkPowerStatus() {
     // 可以通过 wx.getSetting 先查询一下用户是否授权了 "scope.userInfo" 这个 scope
     wx.getSetting({
       success: res => {
@@ -41,7 +39,7 @@ App({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
-              console.log("getUserInfo的基本信息",JSON.stringify(res.userInfo))
+              console.log("getUserInfo的基本信息", JSON.stringify(res.userInfo))
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
@@ -95,25 +93,25 @@ App({
       }
     })
   },
-  updataApp: function () {//版本更新
+  updataApp: function() { //版本更新
     if (wx.canIUse('getUpdateManager')) {
       const updateManager = wx.getUpdateManager()
-      updateManager.onCheckForUpdate(function (res) {
+      updateManager.onCheckForUpdate(function(res) {
         console.log("updataAppres", JSON.stringify(res))
         if (res.hasUpdate) { // 请求完新版本信息的回调
-          updateManager.onUpdateReady(function () {
+          updateManager.onUpdateReady(function() {
             wx.showModal({
               title: '更新提示',
               content: '新版本已经准备好，是否重启应用？',
-              success: function (res) {
-                if (res.confirm) {// 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+              success: function(res) {
+                if (res.confirm) { // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
                   updateManager.applyUpdate()
                 }
               }
             })
           })
-          updateManager.onUpdateFailed(function () {
-            wx.showModal({// 新的版本下载失败
+          updateManager.onUpdateFailed(function() {
+            wx.showModal({ // 新的版本下载失败
               title: '已经有新版本了哟~',
               content: '新版本已经上线啦~，请您删除当前小程序，重新搜索打开哟~',
             })
@@ -122,7 +120,7 @@ App({
       })
     } else {
       console.log("alile ")
-      wx.showModal({// 如果希望用户在最新版本的客户端上体验您的小程序，可以这样子提示
+      wx.showModal({ // 如果希望用户在最新版本的客户端上体验您的小程序，可以这样子提示
         title: '提示',
         content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
       })
--- pages/authorization/authorization.js
+++ pages/authorization/authorization.js
@@ -1,11 +1,14 @@
 // pages/authorization/authorization.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
   /**
@@ -14,8 +17,45 @@ Page({
   onLoad: function (options) {
     wx.hideShareMenu();
     console.log("start plan")
+    this.checkUserInfo()
   },
-
+  checkUserInfo () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      console.log("来这里了")
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  authorize: function (e) {
+    console.log("getUserInfo",e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
