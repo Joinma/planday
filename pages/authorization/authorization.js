a37dcc0a67097ac1aa94e8b9a477cd1fc6ada4ce
Joinma <18814094575@163.com>
2019-01-27 01:59:11 +0800

    feat:加载动画组件

--- pages/authorization/authorization.js
+++ pages/authorization/authorization.js
@@ -18,7 +18,7 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     wx.hideShareMenu();
     if (app.globalData.userInfo) {
       this.checkUserStatus()
@@ -79,12 +79,12 @@ Page({
     }
   },
   // 跳转首页
-  redirectToIndex: function () {
+  redirectToIndex: function() {
     wx.switchTab({
       url: '/pages/index/index',
     })
   },
-  authorize: function (e) {
+  authorize: function(e) {
     if (e.detail.userInfo) {
       app.globalData.userInfo = e.detail.userInfo
       this.setData({
@@ -103,49 +103,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
